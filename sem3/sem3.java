76a2a083be54903ac4732d87e0aface473e1b829
Антон Ефимов <Di_oXid@mail.ru>
2023-04-06 19:11:58 +0500

    add reverce

--- sem3/sem3.java
+++ sem3/sem3.java
@@ -1,23 +1,29 @@
 package sem3;
 
 /**
- * sem3
+ * sem3 HW
  */
 public class sem3 {
 
     public static void main(String[] args) {
 
-        MylinkedList list = new MylinkedList();
-        list.append(1);
-        list.append(3);
-        list.append(6);
-        list.append(43);
-        list.append(331);
-        list.append(765);
-        list.append(788);
-        list.append(967);
-        list.append(999);
-        System.out.println(list.toString());
+        MylinkedList list1 = new MylinkedList();
+        list1.addEnd(1);
+        list1.addEnd(3);
+        list1.addEnd(6);
+        list1.addEnd(43);
+        list1.addEnd(331);
+        list1.addEnd(765);
+        list1.addEnd(788);
+        list1.addEnd(967);
+        list1.addEnd(999);
+        list1.addFirst(-127);
+        list1.addFirst(-235);
+        list1.addFirst(-345);
+        list1.addFirst(-567);
+        System.out.println(list1.toString());
+        MylinkedList reveseList = MylinkedList.reverse(list1);
+        System.out.println(reveseList.toString());
     }
 }
 
@@ -50,12 +56,52 @@ class Node {
 class MylinkedList {
     private Node head;
 
-    public void append(int value) {
+    public void addEnd(int value) {
         Node node = new Node(value);
         if (this.head == null) {
             this.head = node;
         } else {
             searchEnd().setNext(node);
+        }
+    }
+
+    public void addFirst(int value) {
+        Node addNode = new Node(value);
+        addNode.setNext(head);
+        head = addNode;
+    }
+
+    public static MylinkedList reverse(MylinkedList mylinkedList) {
+        MylinkedList result = new MylinkedList();
+        Node temp = mylinkedList.head;
+        while (true) {
+            if (temp.getNext() != null) {
+                result.addFirst(temp.getValue());
+                temp = temp.getNext();
+            } else {
+                result.addFirst(temp.getValue());
+                return result;
+            }
+        }
+    }
+
+    public static MylinkedList copy(MylinkedList mylinkedList) {
+        if (mylinkedList == null) {
+            return null;
+        }
+
+        MylinkedList result = new MylinkedList();
+
+        Node temp = mylinkedList.head;
+
+        while (true) {
+            if (temp.getNext() != null) {
+                result.addEnd(temp.getValue());
+                temp = temp.getNext();
+            } else {
+                result.addEnd(temp.getValue());
+                return result;
+            }
         }
     }
 
